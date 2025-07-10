602fae4c2974c2184a5db35d5654528c4b76f1c9
leviutima <levi.utima@gmail.com>
2025-07-10 01:53:19 -0300

    refactor: removing header shown

--- App.tsx
+++ App.tsx
@@ -12,8 +12,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-          <Stack.Screen name="login" component={Login} />
-          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="login" component={Login} options={{ headerShown: false }}/>
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
 
